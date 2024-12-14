0c87f55261a8da645a1e86ff111907baa0b6e93f
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-12-14 09:59:53 +0200

    added home bg image

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-     <p className="text-8xl font-bold">E-JUST Powerlifting Contest</p>
+    <div className="relative flex flex-col items-center justify-center h-screen">
+      <Image
+        src="/globe-outline-dark-2.svg"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+
+        quality={100}
+        className="-z-10 opacity-20 blur-sm"
+      />
+
+      <p className="text-8xl font-bold text-white drop-shadow-xl">
+        E-JUST Powerlifting Contest
+      </p>
     </div>
   );
 }
